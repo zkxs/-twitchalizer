7f5a91ed4f7ad8ac6e722874c29736ae9cd29574
Michael Ripley <zkxs00@gmail.com>
2017-12-17 19:03:22 -0600

    Remove the @JsonProperty annotations
    they break things for some reason

--- src/main/scala/net/michaelripley/twitchalizer/dto/twitch/livegames/LiveGames.scala
+++ src/main/scala/net/michaelripley/twitchalizer/dto/twitch/livegames/LiveGames.scala
@@ -1,8 +1,6 @@
 package net.michaelripley.twitchalizer.dto.twitch.livegames
 
-import com.fasterxml.jackson.annotation.JsonProperty
-
 case class LiveGames(
-  @JsonProperty("_total") total: Int,
-  @JsonProperty("follows") games: Array[Game]
+  _total: Int,
+  follows: Seq[Game]
 )
